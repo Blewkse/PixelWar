be34fea03b438973a23a0b5b60ff759905805d79
jules <jules.bloux@etu.uca.fr>
2022-04-11 19:10:16 +0200

    ajout du modificateur de couleur: problème pour le faire disparaitre

--- src/App/Grid/Pixel.tsx
+++ src/App/Grid/Pixel.tsx
@@ -1,15 +1,29 @@
 import React, { useCallback, useState } from "react";
+import { HexColorPicker } from 'react-colorful';
+
 
 function Pixel() {
 
+    const [editColor, seteditColor] = useState<boolean>(false)
     const [color, setColor] = useState("#FFFFFF");
+    var Color
 
     const handleClick = useCallback(() => {
-        setColor("#FFBB77")
+        (editColor) ? seteditColor(false) : seteditColor(true);
+        console.log(editColor)
     }, []);
     
     return (
-        <div className="cursor-pointer aspect-square" style={{background: color}} onClick={handleClick}>
+        <div className="cursor-pointer aspect-square relative" style={{background: color}} onClick={handleClick} >
+            {editColor? (
+                <div className=" absolute top-0 left-full  z-10 justify-center" > 
+                <HexColorPicker color="color" onChange={setColor} />
+                <button className="bg-slate-400 rounded" onClick = {() => seteditColor(false)} >Entrer</button>
+                </div>
+            ):null
+            
+
+            }
             
         </div>
     );
